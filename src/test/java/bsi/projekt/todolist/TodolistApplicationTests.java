c1e9e3b14731cd59105e4590b8cab7b601c8d550
LPIETRZYKOWSKI <lukasz.pietrzykowski12@gmail.com>
2022-04-03 16:45:55 +0200

    initial

---
+++ src/test/java/bsi/projekt/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package bsi.projekt.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
